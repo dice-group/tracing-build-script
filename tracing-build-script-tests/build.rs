551c055ebce3a2b4a32b6a878622cb49b9b00f7a
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2025-02-13 12:11:39 +0100

    fix bugs related to special chars in log messages and add tests via separate crate in workspace

---
+++ tracing-build-script-tests/build.rs
@@ -0,0 +1,25 @@
+use tracing::metadata::LevelFilter;
+
+fn write_message(msg: &str) {
+    tracing::trace!("{msg}");
+    tracing::debug!("{msg}");
+    tracing::info!("{msg}");
+    tracing::warn!("{msg}");
+    tracing::error!("{msg}");
+}
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_writer(tracing_build_script::BuildScriptMakeWriter)
+        .with_ansi(false)
+        .without_time()
+        .with_max_level(LevelFilter::TRACE)
+        .init();
+
+    write_message("simple message");
+    write_message("with\nnew\nline");
+    write_message("with\nnew\nlineend\n");
+    write_message("\nwith\nnew\nlinestart");
+    write_message("two\n\nnewlines");
+    write_message("other\rspecial\tchar\0a\tb\"c\\");
+}
